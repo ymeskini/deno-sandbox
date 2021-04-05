5724aea2102e7f73235fec0157b3818d6364d160
Youssef Meskini <youss.meskini@gmail.com>
2021-04-05 11:05:09 +0100

    fixed version of oak

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v6.5.0/mod.ts";
 
 const v1Router = new Router();
 
