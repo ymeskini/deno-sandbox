3008f8f7f8cf0970246766a5f94d7e3b7ff1fe38
Youssef Meskini <youss.meskini@gmail.com>
2021-04-05 11:08:37 +0100

    simple web server

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
-import { Application, Router } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+import { serve } from "https://deno.land/std@0.92.0/http/server.ts";
 
-const v1Router = new Router();
+const server = serve({ hostname: "0.0.0.0", port: 8080 });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
 
-v1Router.get("/", (context) => {
-  context.response.body = "We're up yeaah and running";
-});
+for await (const request of server) {
+  let bodyContent = "Your user-agent is:\n\n";
+  bodyContent += request.headers.get("user-agent") || "Unknown";
 
-const app = new Application();
-
-app.use(v1Router.routes());
-await app.listen({ port: 8000 });
+  request.respond({ status: 200, body: bodyContent });
+}
